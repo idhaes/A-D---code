060c682240d4af77ca7e2bcec449142c2ebd1559
idhaes <Ilyan.DeHaes@UGent.be>
2025-11-16 17:42:03 +0100

    Refactor LinkedList methods for consistency

--- Lecture5/LinkedList.py
+++ Lecture5/LinkedList.py
@@ -20,38 +20,37 @@ class LinkedList:
 
     # Add an element to the beginning of the list 
     def addFirst(self, e):
-        newNode = Node(e) # Create a new node
-        newNode.next = self.__head # link the new node with the head
-        self.__head = newNode # head points to the new node
-        self.__size += 1 # Increase list size
+        newNode = Node(e)
+        newNode.next = self.__head
+        self.__head = newNode
+        self.__size += 1
 
-        if self.__tail == None: # the new node is the only node in list
+        if self.__tail == None:
             self.__tail = self.__head
 
     # Add an element to the end of the list 
     def addLast(self, e):
-        newNode = Node(e) # Create a new node for e
+        newNode = Node(e)
     
         if self.__tail == None:
-            self.__head = self.__tail = newNode # The only node in list
+            self.__head = self.__tail = newNode
         else:
-            self.__tail.next = newNode # Link the new with the last node
-            self.__tail = self.__tail.next # tail now points to the last node
+            self.__tail.next = newNode
+            self.__tail = self.__tail.next
     
-        self.__size += 1 # Increase size
+        self.__size += 1
 
     # Same as addLast 
     def add(self, e):
         self.addLast(e)
 
-    # Insert a new element at the specified index in this list
-    # The index of the head element is 0 
+    # Insert element at index
     def insert(self, index, e):
         if index == 0:
-            self.addFirst(e) # Insert first
+            self.addFirst(e)
         elif index >= self.__size:
-            self.addLast(e) # Insert last
-        else: # Insert in the middle
+            self.addLast(e)
+        else:
             current = self.__head
             for i in range(1, index):
                 current = current.next
@@ -60,135 +59,171 @@ class LinkedList:
             (current.next).next = temp
             self.__size += 1
 
-    # Remove the head node and
-    #  return the object that is contained in the removed node. 
+    # Remove first element
     def removeFirst(self):
         if self.__size == 0:
-            return None # Nothing to delete
+            return None
         else:
-            temp = self.__head # Keep the first node temporarily
-            self.__head = self.__head.next # Move head to point the next node
-            self.__size -= 1 # Reduce size by 1
-            if self.__head == None: 
-                self.__tail = None # List becomes empty 
-            return temp.element # Return the deleted element
+            temp = self.__head
+            self.__head = self.__head.next
+            self.__size -= 1
+            if self.__head == None:
+                self.__tail = None
+            return temp.element
 
-    # Remove the last node and
-    # return the object that is contained in the removed node
+    # Remove last element
     def removeLast(self):
         if self.__size == 0:
-            return None # Nothing to remove
-        elif self.__size == 1: # Only one element in the list
+            return None
+        elif self.__size == 1:
             temp = self.__head
-            self.__head = self.__tail = None  # list becomes empty
+            self.__head = self.__tail = None
             self.__size = 0
             return temp.element
         else:
             current = self.__head
-        
             for i in range(self.__size - 2):
                 current = current.next
-        
             temp = self.__tail
             self.__tail = current
             self.__tail.next = None
             self.__size -= 1
             return temp.element
 
-    # Remove the element at the specified position in this list.
-    #  Return the element that was removed from the list. 
+    # Remove element at index
     def removeAt(self, index):
         if index < 0 or index >= self.__size:
-            return None # Out of range
+            return None
         elif index == 0:
-            return self.removeFirst() # Remove first 
+            return self.removeFirst()
         elif index == self.__size - 1:
-            return self.removeLast() # Remove last
+            return self.removeLast()
         else:
             previous = self.__head
-    
             for i in range(1, index):
                 previous = previous.next
-        
             current = previous.next
             previous.next = current.next
             self.__size -= 1
             return current.element
 
-    # Return true if the list is empty
     def isEmpty(self):
         return self.__size == 0
-    
-    # Return the size of the list
+
     def getSize(self):
         return self.__size
 
     def __str__(self):
         result = "["
-
         current = self.__head
         for i in range(self.__size):
             result += str(current.element)
             current = current.next
             if current != None:
-                result += ", " # Separate two elements with a comma
+                result += ", "
             else:
-                result += "]" # Insert the closing ] in the string
-
+                result += "]"
         return result
 
-    # Clear the list */
     def clear(self):
         self.__head = self.__tail = None
+        self.__size = 0
 
-    # Return true if this list contains the element o 
+    # -------------------------
+    # COMPLETED FUNCTIONS BELOW
+    # -------------------------
+
+    # Return true if the list contains element e
     def contains(self, e):
-        print("Implementation left as an exercise")
-        return True
+        current = self.__head
+        while current != None:
+            if current.element == e:
+                return True
+            current = current.next
+        return False
 
-    # Remove the element and return true if the element is in the list 
+    # Remove the first occurrence of e (return True if removed)
     def remove(self, e):
-        print("Implementation left as an exercise")
-        return True
+        if self.__size == 0:
+            return False
 
-    # Return the element from this list at the specified index 
+        # Case: first element matches
+        if self.__head.element == e:
+            self.removeFirst()
+            return True
+
+        previous = self.__head
+        current = self.__head.next
+
+        while current != None:
+            if current.element == e:
+                previous.next = current.next
+                if current == self.__tail:
+                    self.__tail = previous
+                self.__size -= 1
+                return True
+            previous = current
+            current = current.next
+
+        return False
+
+    # Return element at index
     def get(self, index):
-        print("Implementation left as an exercise")
-        return None
+        if index < 0 or index >= self.__size:
+            return None
+        current = self.__head
+        for i in range(index):
+            current = current.next
+        return current.element
 
-    # Return the index of the head matching element in this list.
-    # Return -1 if no match.
+    # Return index of first occurrence of e, or -1
     def indexOf(self, e):
-        print("Implementation left as an exercise")
-        return 0
+        index = 0
+        current = self.__head
+        while current != None:
+            if current.element == e:
+                return index
+            current = current.next
+            index += 1
+        return -1
 
-    # Return the index of the last matching element in this list
-    #  Return -1 if no match. 
+    # Return index of last occurrence of e, or -1
     def lastIndexOf(self, e):
-        print("Implementation left as an exercise")
-        return 0
+        index = 0
+        last = -1
+        current = self.__head
+        while current != None:
+            if current.element == e:
+                last = index
+            current = current.next
+            index += 1
+        return last
 
-    # Replace the element at the specified position in this list
-    #  with the specified element. */
+    # Replace element at index
     def set(self, index, e):
-        print("Implementation left as an exercise")
-        return None
-    
-    # Return elements via indexer
+        if index < 0 or index >= self.__size:
+            return None
+        current = self.__head
+        for i in range(index):
+            current = current.next
+        old = current.element
+        current.element = e
+        return old
+
     def __getitem__(self, index):
         return self.get(index)
 
-    # Return an iterator for a linked list
     def __iter__(self):
         return LinkedListIterator(self.__head)
-    
-# The Node class
+
+
 class Node:
     def __init__(self, e):
         self.element = e
         self.next = None
 
-class LinkedListIterator: 
+
+class LinkedListIterator:
     def __init__(self, head):
         self.current = head
         
@@ -198,5 +233,4 @@ class LinkedListIterator:
         else:
             element = self.current.element
             self.current = self.current.next
-            return element    
-        
+            return element
